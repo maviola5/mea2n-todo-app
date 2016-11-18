5fb38ff25c7457578dba7f342fc1004fa4a59dce
Tony Viola <maviola5@gmail.com>
2016-11-18 00:25:44 -0500

    init app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,17 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { Routes, RouterModule, ActivatedRoute } from '@angular/router';
+
 import { AppComponent } from './app.component';
+
+const routes: Routes = [
+  {},
+  {},
+  {},
+  { path: '', redirectTo: '', pathMatch: 'full'},
+  { path: '**', component: PageNotFoundComponent}
+];
 
 @NgModule({
   declarations: [
