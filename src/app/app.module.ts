3424938f27f4ecd0fa350d5c7d79d812c6e65e15
Tony V <maviola5@gmail.com>
2016-11-18 20:55:05 -0500

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,28 +3,41 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { LocationStrategy, PathLocationStrategy } from '@angular/common';
+
 import { Routes, RouterModule, ActivatedRoute } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { TodoComponent } from './todo/todo.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
-  {},
-  {},
-  {},
-  { path: '', redirectTo: '', pathMatch: 'full'},
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'todo', component: TodoComponent },
+  { path: '', redirectTo: 'todo', pathMatch: 'full'},
   { path: '**', component: PageNotFoundComponent}
 ];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageNotFoundComponent,
+    TodoComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [
+    { provide: LocationStrategy, useClass: PathLocationStrategy }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
